2afe306bce51ce937ea9f529794cc6c59c340c97
Francisco <franciso.m.estrela@gmail.com>
2026-04-07 11:16:36 +0100

    Correção de bugs no som;

--- lib/services/sound_service.dart
+++ lib/services/sound_service.dart
@@ -1,21 +1,55 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundService {
-  static final AudioPlayer _player = AudioPlayer();
+  // Um player por tipo de som para evitar conflitos
+  static final AudioPlayer _correctPlayer = AudioPlayer();
+  static final AudioPlayer _wrongPlayer   = AudioPlayer();
+  static final AudioPlayer _victoryPlayer = AudioPlayer();
+  static final AudioPlayer _failPlayer    = AudioPlayer();
 
-  static Future<void> _play(String fileName) async {
-    try {
-      await _player.setReleaseMode(ReleaseMode.stop);
-      await _player.setVolume(1.0);
-      await _player.stop();
-      await _player.play(AssetSource(fileName));
-    } catch (e) {
-      print('SoundService error: $e');
-    }
+  static Future<void> _configure(AudioPlayer player) async {
+    await player.setReleaseMode(ReleaseMode.stop);
+    await player.setVolume(1.0);
+    await player.setAudioContext(AudioContext(
+      android: AudioContextAndroid(
+        isSpeakerphoneOn: false,
+        stayAwake: false,
+        contentType: AndroidContentType.music,
+        usageType: AndroidUsageType.media,
+        audioFocus: AndroidAudioFocus.gainTransientMayDuck,
+      ),
+      iOS: AudioContextIOS(
+        category: AVAudioSessionCategory.playback,
+        options: {AVAudioSessionOptions.mixWithOthers},
+      ),
+    ));
   }
 
-  static Future<void> playCorrect()  async => await _play('sounds/correct.wav');
-  static Future<void> playWrong()    async => await _play('sounds/wrong.wav');
-  static Future<void> playVictory()  async => await _play('sounds/victory.wav');
-  static Future<void> playFail()     async => await _play('sounds/fail.wav');
+  static Future<void> playCorrect() async {
+    try {
+      await _configure(_correctPlayer);
+      await _correctPlayer.play(AssetSource('sounds/correct.wav'));
+    } catch (e) { print('SoundService: $e'); }
+  }
+
+  static Future<void> playWrong() async {
+    try {
+      await _configure(_wrongPlayer);
+      await _wrongPlayer.play(AssetSource('sounds/wrong.wav'));
+    } catch (e) { print('SoundService: $e'); }
+  }
+
+  static Future<void> playVictory() async {
+    try {
+      await _configure(_victoryPlayer);
+      await _victoryPlayer.play(AssetSource('sounds/victory.wav'));
+    } catch (e) { print('SoundService: $e'); }
+  }
+
+  static Future<void> playFail() async {
+    try {
+      await _configure(_failPlayer);
+      await _failPlayer.play(AssetSource('sounds/fail.wav'));
+    } catch (e) { print('SoundService: $e'); }
+  }
 }
